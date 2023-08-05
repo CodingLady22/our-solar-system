ecd8b547afddf7e3994ec7ff492b52936d94dace
Maye Jesuorobo <nayawes@yahoo.com>
2023-08-06 00:18:25 +0200

    set up the router for the app

---
+++ route/home.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const router = Router();
+// import { getPlanets, getMoons } from "../controllers/planets.js";
+
+// * Handles GET request from the homepage
+// router.get("/", getPlanets); // read
+// router.get("/moon", getMoons); // read
+
+export default router;
