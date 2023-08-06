6934141134c545ae13f4037e611b5b59d316201c
Maye Jesuorobo <nayawes@yahoo.com>
2023-08-06 22:39:46 +0200

    setup the schema for the moons

---
+++ models/Moon.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const moonSchema = new Schema({
+    name: {
+        type: String,
+    },
+    diameter: {
+        type: Number,
+    },
+    history: {
+        type: String,
+    },
+    planetName: {
+        type: String,
+    }
+});
+
+const Moon = mongoose.model("Moon", moonSchema);
+
+export default Moon;
