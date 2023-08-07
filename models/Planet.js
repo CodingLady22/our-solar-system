6095062d2c4035bfab3072dc57b76e7541fdd576
Maye Jesuorobo <nayawes@yahoo.com>
2023-08-07 16:35:06 +0200

    set up the schema for the planets

---
+++ models/Planet.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const planetSchema = new Schema({
+    name: {
+        type: String,
+    },
+    size: {
+        type: Number,
+    },
+    distanceFromSun: {
+        type: Number,
+    },
+    facts: {
+        type: [String],
+    },
+    State: {
+        type: String,
+    enum: {
+        values: ['Solid', 'Gas'],
+        // This message can be shown if an unsupported value is entered
+        message: '{VALUE} is not supported',
+    },
+    },
+    moons: [{type: Schema.Types.ObjectId, ref: "Moon"}]
+});
+
+const Planet = mongoose.model("Planet", planetSchema);
+
+export default Planet;
