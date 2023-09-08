78ee8e7df63c83233966828c12b0dba43389fb2a
Maye Jesuorobo <nayawes@yahoo.com>
2023-09-08 12:43:53 +0200

    added the routes to the controller

--- route/home.js
+++ route/home.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 const router = Router();
-// import { getPlanets, getMoons } from "../controllers/planets.js";
+import { getPlanets, getMoons } from "../controllers/planets.js";
 
 // * Handles GET request from the homepage
-// router.get("/", getPlanets); // read
-// router.get("/moon", getMoons); // read
+router.get("/", getPlanets); // read
+router.get("/moon", getMoons); // read
 
 export default router;
