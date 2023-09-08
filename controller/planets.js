8834583ef08674b5a7c79699f9f576a9b9788fd5
Maye Jesuorobo <nayawes@yahoo.com>
2023-09-08 12:40:44 +0200

    This controller passes the data from the model to the views

---
+++ controller/planets.js
@@ -0,0 +1,22 @@
+import Planets from "../models/Planet.js";
+import Moons from "../models/Moon.js";
+
+//* GET: getting the planet info
+export const getPlanets = async (req, res) => {
+    try {
+        const planets = await Planets.find();
+        res.status(200).json(planets);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+//* GET: getting the moon info
+export const getMoons = async (req, res) => {
+    try {
+        const moons = await Moons.find();
+        res.status(200).json(moons);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
