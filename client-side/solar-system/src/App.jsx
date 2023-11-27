a38f834ebd81aa0da52b3619150ba7b95d8b9e84
Maye Jesuorobo <nayawes@yahoo.com>
2023-11-27 14:19:00 +0100

    import routes and routers and link the home page to earth page

--- client-side/solar-system/src/App.jsx
+++ client-side/solar-system/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './components/home/Main'
+import Earth2 from './Earth2';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/earth/3' element={<Earth2 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
